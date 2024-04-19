9c86b91e0a27ebb6269a5ac3b96c107465ebca9e
David Clarke <davidclarke@zebracrossing.co.nz>
2024-04-19 18:43:50 +1200

    challenge 2.3 add user details

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,19 +1,48 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import data from "../../static.json";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function BookablesList() {
     const { users } = data;
     const [usersIndex, setUsersIndex] = useState(0);
+    const user = users[usersIndex];
+
+    function nextUser() {
+        setUsersIndex((i) => (i + 1) % users.length);
+    }
 
     return (
-        <ul className="items-list-nav">
-            {users.map((u, i) => (
-                <li key={u.id} className={i === usersIndex ? "selected" : null}>
-                    <button className="btn" onClick={() => setUsersIndex(i)}>
-                        {u.name}
+        <Fragment>
+            <div>
+                <ul className="items-list-nav">
+                    {users.map((u, i) => (
+                        <li key={u.id} className={i === usersIndex ? "selected" : null}>
+                            <button className="btn" onClick={() => setUsersIndex(i)}>
+                                {u.name}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+                <p>
+                    <button className="btn" onClick={nextUser}>
+                        <FaArrowRight />
+                        <span>Next</span>
                     </button>
-                </li>
-            ))}
-        </ul>
+                </p>
+
+            </div>
+            {user && (
+                <div className="bookable-details">
+                    <div className="item">
+                        <div className="item-header">
+                            <h2>{user.name}</h2>
+                        </div>
+                        <h3>{user.title}</h3>
+                        <p>{user.notes}</p>
+
+                    </div>
+                </div >
+            )}
+        </Fragment>
     );
 }
