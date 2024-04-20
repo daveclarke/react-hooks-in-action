4e90bd01a7c9b2ad92d839384ce2844c1ec1f6a2
David Clarke <davidclarke@zebracrossing.co.nz>
2024-04-20 13:52:15 +1200

    3.3.4 passing init function to useReducer hook

--- src/components/Bookings/BookingsPage.js
+++ src/components/Bookings/BookingsPage.js
@@ -1,7 +1,10 @@
+import WeekPicker from "./WeekPicker";
+
 export default function BookingsPage() {
     return (
         <main className="bookings-page">
-            <h1>Bookings</h1>
+            <p>Bookings</p>
+            <WeekPicker date={new Date()} />
         </main>
     );
 }
