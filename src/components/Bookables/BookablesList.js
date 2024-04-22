8233e07ca4deaa3659ea2b162a1e956193d2291b
David Clarke <davidclarke@zebracrossing.co.nz>
2024-04-22 19:28:06 +1200

    6.4 sharing state and updater fn from useState

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,35 +1,48 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import getData from "../../utils/api";
 import Spinner from "../UI/Spinner";
 
 
-export default function BookablesList({ state, dispatch }) {
-    const { group, bookablesIndex, bookables, isLoading, error } = state;
+export default function BookablesList({ bookable, setBookable }) {
+    const [bookables, setBookables] = useState([]);
+    const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const group = bookable?.group;
     const bookablesInGroup = bookables.filter(b => b.group === group);
     const groups = [...new Set(bookables.map(b => b.group))];
     const nextButtonRef = useRef();
 
     // run once
     useEffect(() => {
-        dispatch({ type: "FETCH_BOOKABLES_REQUEST" });
         getData("http://localhost:3001/bookables")
-            .then(bookables => dispatch({ type: "FETCH_BOOKABLES_SUCCESS", payload: bookables }))
-            .catch(error => dispatch({ type: "FETCH_BOOKABLES_ERROR", payload: error }))
-    }, [dispatch]);
+            .then(bookables => {
+                setBookable(bookables[0]);
+                setBookables(bookables);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setIsLoading(false);
+            });
+    }, [setBookable]);
 
 
     function changeGroup(e) {
-        dispatch({ type: "SET_GROUP", payload: e.target.value });
+        const bookablesInSelectedGroup = bookables.filter(b => b.group === e.target.value);
+        setBookable(bookablesInSelectedGroup[0]);
     }
 
-    function changeBookable(i) {
-        dispatch({ type: "SET_BOOKABLE", payload: i });
+    function changeBookable(selectedBookable) {
+        setBookable(selectedBookable);
         nextButtonRef.current.focus();
     }
 
     function nextBookable() {
-        dispatch({ type: "NEXT_BOOKABLE" });
+        const i = bookablesInGroup.indexOf(bookable);
+        const nextIndex = (i + 1) % bookablesInGroup.length;
+        const nextBookable = bookablesInGroup[nextIndex];
+        setBookable(nextBookable);
     }
 
     if (error) return <p>{error.message}</p>;
@@ -42,9 +55,9 @@ export default function BookablesList({ state, dispatch }) {
                 {groups.map(g => <option value={g} key={g}>{g}</option>)}
             </select>
             <ul className="bookables items-list-nav">
-                {bookablesInGroup.map((b, i) => (
-                    <li key={b.id} className={i === bookablesIndex ? "selected" : null}>
-                        <button className="btn" onClick={() => changeBookable(i)}>
+                {bookablesInGroup.map(b => (
+                    <li key={b.id} className={b.id === bookable.id ? "selected" : null}>
+                        <button className="btn" onClick={() => changeBookable(b)}>
                             {b.title}
                         </button>
                     </li>
--- src/components/Bookables/BookablesView.js
+++ src/components/Bookables/BookablesView.js
@@ -1,25 +1,13 @@
-import { useReducer, Fragment } from "react";
+import { useState, Fragment } from "react";
 import BookablesList from "./BookablesList";
 import BookablesDetails from "./BookablesDetails";
-import reducer from "./reducer";
-
-const initialState = {
-    group: "Rooms",
-    bookablesIndex: 0,
-    bookables: [],
-    isLoading: true,
-    error: false
-};
 
 export default function BookablesView() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const bookablesInGroup = state.bookables.filter(b => b.group === state.group);
-    const bookable = bookablesInGroup[state.bookablesIndex];
+    const [bookable, setBookable] = useState();
 
     return (
         <Fragment>
-            <BookablesList state={state} dispatch={dispatch} />
+            <BookablesList bookable={bookable} setBookable={setBookable} />
             <BookablesDetails bookable={bookable} />
         </Fragment>
     )
