a32ea23469b409ef9530bdc416ec541f41341364
David Clarke <davidclarke@zebracrossing.co.nz>
2024-04-24 08:58:54 +1200

    6.5.2 maintaining fn identity with useCallback

--- src/components/Bookables/BookablesView.js
+++ src/components/Bookables/BookablesView.js
@@ -1,13 +1,20 @@
-import { useState, Fragment } from "react";
+import { useState, useCallback, Fragment } from "react";
 import BookablesList from "./BookablesList";
 import BookablesDetails from "./BookablesDetails";
 
 export default function BookablesView() {
     const [bookable, setBookable] = useState();
 
+    const updateBookable = useCallback(selected => {
+        if (selected) {
+            selected.lastShown = Date.now();
+            setBookable(selected);
+        }
+    }, []);
+
     return (
         <Fragment>
-            <BookablesList bookable={bookable} setBookable={setBookable} />
+            <BookablesList bookable={bookable} setBookable={updateBookable} />
             <BookablesDetails bookable={bookable} />
         </Fragment>
     )
