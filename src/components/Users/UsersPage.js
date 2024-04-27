567f36accb59489007682f2f9dd91369b3372907
David Clarke <davidclarke@zebracrossing.co.nz>
2024-04-28 09:54:42 +1200

    challenge 8.1

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,16 +1,20 @@
-import { useState } from "react"; // import useState
+import { useState, useContext } from "react"; // import useState
 import UsersList from "./UsersList";
 import UserDetails from "./UserDetails"; // import new component
+import UserContext from "./UserContext";
 
 export default function UsersPage() {
     // manage selected user state
     const [user, setUser] = useState(null);
+    const loggedInUser = useContext(UserContext);
+    const currentUser = user || loggedInUser;
+
 
     // pass user state down
     return (
         <main className="users-page">
-            <UsersList user={user} setUser={setUser} />
-            <UserDetails user={user} />
+            <UsersList user={currentUser} setUser={setUser} />
+            <UserDetails user={currentUser} />
         </main>
     );
 }
