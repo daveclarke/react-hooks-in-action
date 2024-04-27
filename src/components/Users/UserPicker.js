4bbb1a46b2d2448454dde7f163d397601dd76a23
David Clarke <davidclarke@zebracrossing.co.nz>
2024-04-28 09:29:32 +1200

    8.1.4 display edit button

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,21 +1,30 @@
 import { useState, useEffect } from "react";
 import Spinner from "../UI/Spinner";
 
-export default function UserPicker() {
+export default function UserPicker({ user, setUser }) {
     const [users, setUsers] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:3001/users")
             .then((resp) => resp.json())
-            .then((data) => setUsers(data));
-    }, []);
+            .then((data) => {
+                setUsers(data);
+                setUser(data[0])
+            });
+    }, [setUser]);
+
+    function handleSelect(e) {
+        const selectedId = parseInt(e.target.value, 10);
+        const selectedUser = users.find(u => u.id === selectedId);
+        setUser(selectedUser);
+    }
 
     if (users === null) return <Spinner />;
 
     return (
-        <select>
+        <select className="user-picker" onChange={handleSelect} value={user?.id}>
             {users.map(u => (
-                <option key={u.id}>{u.name}</option>
+                <option key={u.id} value={u.id}>{u.name}</option>
             ))}
         </select>
     );
